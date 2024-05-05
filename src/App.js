91b773cc82a8371117832ebcea7704b6a51e27c6
Kareem Ayman <kareemayman8@gmail.com>
2024-05-05 19:33:35 +0300

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
   };
 
   return (
-    <div class="chat-container bg-gradient-to-r from-indigo-500 to-purple-600  max-h-full rounded-xl shadow-md p-4 flex flex-col h-screen overflow-y-auto">
+    <div class="chat-container bg-gradient-to-r from-indigo-500 to-purple-600  max-h-full rounded-xl shadow-md flex flex-col h-screen overflow-y-auto px-10">
       <h2 class="text-5xl text-white font-bold mb-4 text-center">Chat with EdaraBot</h2>
       <div className='w-1/2 m-auto '>
         <div class="flex flex-col gap-1 overflow-y-auto max-h-full max-w-full text-white text-xl items-center justify-center">
@@ -50,11 +50,11 @@ function App() {
             </div>
           ))}
         </div>
-        <div class="flex items-center justify-center gap-2 pt-20">
+        <div class="flex items-center justify-center gap-2 p-20 ">
 
           <input
             type="text"
-            class="rounded-md px-20 py-2 bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            class="rounded-md px-10 py-2 bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500"
             value={userInput}
             onChange={handleUserInput}
             onKeyPress={handleKeyPress}
