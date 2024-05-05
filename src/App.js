eba3cc966f736945895da888db2f6416dff4abdc
Kareem Ayman <kareemayman8@gmail.com>
2024-05-05 19:55:07 +0300

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import './App.css';
 function App() {
-  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("مرحباً، يسعدني مساعدتك اليوم. كيف يمكنني مساعدتك؟");
   const [userInput, setUserInput] = useState('');
 
   const handleUserInput = (event) => {
@@ -11,25 +11,15 @@ function App() {
 
   async function sendMessage() {
     if (userInput.trim() !== '') {
-      // Update messages with user input
-      const newMessage = {
-        sender: 'user',
-        text: "Loading",
-      };
-      setMessages([...messages, newMessage]);
-      setUserInput('');
+      setMessage("Loading....");
       const genAI = new GoogleGenerativeAI('AIzaSyDlAZg508ARGhDkCyIuJUu7X1AnLO8W9Rw');
       const model = genAI.getGenerativeModel({ model: "gemini-pro" });
       const prompt = userInput;
       const result = await model.generateContent(prompt);
       const response = await result.response;
       const text = response.text();
-      const botResponse = {
-        sender: 'bot',
-        text: text,
-      };
       setUserInput("")
-      setMessages([...messages, botResponse]);
+      setMessage(text);
     }
   };
 
@@ -40,25 +30,23 @@ function App() {
   };
 
   return (
-    <div class="chat-container bg-gradient-to-r from-indigo-500 to-purple-600  max-h-full rounded-xl shadow-md flex flex-col h-screen overflow-y-auto px-10">
+    <div class="chat-container bg-gradient-to-r from-indigo-500 to-purple-600  max-h-full rounded-xl shadow-md flex flex-col h-screen overflow-y-auto p-10">
       <h2 class="text-5xl text-white font-bold mb-4 text-center">Chat with EdaraBot</h2>
-      <div className='w-1/2 m-auto '>
-        <div class="flex flex-col gap-1 overflow-y-auto max-h-full max-w-full text-white text-xl items-center justify-center">
-          {messages.map((message) => (
-            <div key={message.text} className={`message ${message.sender}`}>
-              {message.text}
-            </div>
-          ))}
+      <div className='pt-10'>
+        <div class="flex items-center justify-center ">
+          <div class="message-bubble rounded-lg bg-indigo-800 px-4 py-4 text-white shadow-md text-center text-2xl">
+            {message}
+          </div>
         </div>
-        <div class="flex items-center justify-center gap-2 p-20 ">
+        <div class="flex items-center justify-center gap-2 py-10 w-full sm:w-1/2  m-auto ">
 
           <input
             type="text"
-            class="rounded-md px-10 py-2 bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            class="rounded-md py-2 bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 text-center w-full"
             value={userInput}
             onChange={handleUserInput}
             onKeyPress={handleKeyPress}
-            placeholder="Type your message..."
+            placeholder=""
           />
           <button
             class="bg-indigo-500 text-white hover:bg-indigo-700 font-bold py-2 px-4 rounded-md shadow-sm"
