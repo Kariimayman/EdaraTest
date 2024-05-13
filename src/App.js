5de38774da29b28dec54e83ad99c96b8d7a386bf
Kareem Ayman <kareemayman8@gmail.com>
2024-05-13 23:44:48 +0300

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleLogin } from '@react-oauth/google';
 import './App.css';
 function App() {
   const [message, setMessage] = useState("مرحباً، يسعدني مساعدتك اليوم. كيف يمكنني مساعدتك؟");
@@ -12,8 +13,8 @@ function App() {
   async function sendMessage() {
     if (userInput.trim() !== '') {
       setMessage("Loading....");
-      const genAI = new GoogleGenerativeAI('AIzaSyDlAZg508ARGhDkCyIuJUu7X1AnLO8W9Rw');
-      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+      const genAI = new GoogleGenerativeAI('AIzaSyCkM44TR33a0FomGDszGbuBnzyJL1pjg08');
+      const model = genAI.getGenerativeModel({ model: "tunedModels/edarachatbot2" });
       const prompt = userInput;
       const result = await model.generateContent(prompt);
       const response = await result.response;
@@ -28,11 +29,17 @@ function App() {
       sendMessage();
     }
   };
-
+  const responseMessage = (response) => {
+    console.log(response);
+};
+const errorMessage = (error) => {
+    console.log(error);
+};
   return (
     <div class="chat-container bg-gradient-to-r from-indigo-500 to-purple-600  max-h-full rounded-xl shadow-md flex flex-col h-screen overflow-y-auto p-10">
       <h2 class="text-5xl text-white font-bold mb-4 text-center">Chat with EdaraBot</h2>
       <div className='pt-10'>
+        <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
         <div class="flex items-center justify-center ">
           <div class="message-bubble rounded-lg bg-indigo-800 px-4 py-4 text-white shadow-md text-center text-2xl">
             {message}
