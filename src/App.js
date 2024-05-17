d887b56c254f7864b353fdabd79c9df3dbc07fcb
Kareem Ayman <kareemayman8@gmail.com>
2024-05-17 22:33:53 +0300

    ssss

--- src/App.js
+++ src/App.js
@@ -55,7 +55,8 @@ function App() {
       const response = await axios.post(url, data, { headers });
       console.log(response)
       const generatedText = response.data.candidates[0].content.parts[0].text;
-      return generatedText.toString();
+      const generatedTextarray = generatedText.split('\n')
+      return generatedTextarray;
     } catch (error) {
       console.log(error)
       return "something went wrong"
