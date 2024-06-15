25f45059fbafa6ea0738d221e6a046be17cdfd9e
Kareem Ayman <kareemayman8@gmail.com>
2024-06-15 09:23:35 +0300

    ss

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
   const fetchEdaraData = async (prompt) => {
     console.log("fetching data for " + prompt)
     try {
-      const url = 'https://generativelanguage.googleapis.com/v1beta/tunedModels/edarachatbot5:generateContent';
+      const url = 'https://generativelanguage.googleapis.com/v1beta/tunedModels/edarachatbot:generateContent';
       const headers = {
         // 'Content-Type': 'application/json',
         Authorization: `Bearer ${accessToken}`,
